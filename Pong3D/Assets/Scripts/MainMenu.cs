9b8b27b1929b2f5836ac0b9f681dac71edbab001
Tristan <tristan1.laubscher@gmail.com>
2024-09-10 12:44:35 +0200

    Settings Menu without functionality
    Created a settings menu with a ball speed slider but it is not yet functional

--- Pong3D/Assets/Scripts/MainMenu.cs
+++ Pong3D/Assets/Scripts/MainMenu.cs
@@ -5,7 +5,18 @@ public class MainMenu : MonoBehaviour {
 
     public void PlayButtonClicked()
     {
-        SceneManager.LoadScene("Main");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+
+    public void SettingsButtonClicked()
+    {
+
+    }
+
+    public void QuitButtonClicked()
+    {
+        Debug.Log("Game Quit");
+        Application.Quit();
     }
 
 }
